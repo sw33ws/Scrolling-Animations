de58d60dd636dbce7869811f5e959ed38c4e2ea2
sw33ws <gillisl22@outlook.com>
2022-10-28 14:12:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else { // this else makes it so the item dispears again if you look away
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
